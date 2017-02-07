56cb60f3d5f9a23d7b36df62f1324491742c534e
Thomas Bratt <thomas.bratt@gmail.com>
2017-02-07 19:49:49 +0000

    First commit.

---
+++ src/main/java/com/github/thomasbratt/Main.java
@@ -0,0 +1,8 @@
+package com.github.thomasbratt;
+
+// The purpose of this type is to 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Example output text");
+    }
+}
---
+++ src/test/java/com/github/thomasbratt/Test1.java
@@ -0,0 +1,18 @@
+package com.github.thomasbratt;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+// The purpose of this type is to
+public class Test1 {
+
+    @Test
+    public void shouldDoSomethingSpecific() throws Exception {
+        final String actual = "a value";
+        final String expected = "a value";
+        assertThat(actual, is(expected));
+    }
+
+}
