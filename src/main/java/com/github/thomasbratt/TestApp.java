783a47913819abcac0b3bc021235483dfbe6b718
Thomas Bratt <thomas.bratt@gmail.com>
2020-01-05 13:49:39 +0000

    Changed to use shade maven plugin.

--- src/main/java/com/github/thomasbratt/TestApp.java
+++ src/main/java/com/github/thomasbratt/TestApp.java
@@ -1,7 +1,7 @@
 package com.github.thomasbratt;
 
 // The purpose of this type is to 
-public final class Main {
+public final class TestApp {
     public static void main(String[] args) {
         System.out.println("Example output text");
     }
